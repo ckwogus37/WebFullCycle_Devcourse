72ee6a532278852d56eb0737aae5098d0702ac4d
ckwogus37 <ckwogus37@gmail.com>
2024-03-26 22:19:08 +0900

    24.03.26(화) REST API 개발 실습 (2) 및 마크다운 파일이름 수정

--- 웹풀사이클 스프린트 2/youtube-demo/routes/channels.js
+++ 웹풀사이클 스프린트 2/youtube-demo/routes/channels.js
@@ -1,31 +1,42 @@
 // express 모듈 셋팅
 const express = require('express')
-const app = express()
-app.listen(7777)
-app.use(express.json()) // => req.body를 JSON 형식으로 읽을 수 있게 해주는 모듈
+const router = express.Router();
+
+router.use(express.json())
+// expres.json() : req.body를 JSON 형식으로 읽을 수 있게 해주는 모듈
 
 let db = new Map()
 let channelNum = 1 // 하나의 객체를 유니크하게 구별하기 위함
 
-app
-    .route('/channels')
+router
+    .route('/')
     .get((req,res)=>{   // 채널 전체 조회
-        if(db.size){
-            let channels = []
 
+        // 예외 처리 2가지
+        // 1) id가 BODY에 없는 경우 -> 로그인을 하라고 알림
+        // 2) id가 가진 채널이 없는 경우
+
+        let {id} = req.body
+        let channels = []
+
+        if(db.size && id){ // 1) 예외처리 해결
             db.forEach((value, key)=>{
-                channels.push(value)
+                if(id === value.id)
+                    channels.push(value)
             })
-    
-            res.status(200).json(channels) 
+
+            if(channels.length){ // 2) 예외처리 해결
+                res.status(200).json(channels) 
+            }else{
+                notFoundChannel()
+            }
         }else{
-            res.status(404).json({
-                message : "조회할 채널이 없습니다."
-            })
+            notFoundChannel()
         }
 
     }) 
     .post((req,res)=>{  // 채널 개별 생성 = db에 저장
+
         if(req.body.channelTitle){
             db.set(channelNum++, req.body)
 
@@ -39,8 +50,8 @@ app
         }
     }) 
     
-app    
-    .route('/channels/:channelNum')
+router    
+    .route('/:channelNum')
     .get((req,res)=>{   // 채널 개별 조회
         let {channelNum} = req.params
         channelNum = parseInt(channelNum)
@@ -49,9 +60,7 @@ app
         if(channel){
             res.status(200).json(channel)
         }else{
-            res.status(404).json({
-                message : "채널 정보를 찾을 수 없습니다."
-            })
+            notFoundChannel()
         }
     }) 
     .put((req,res)=>{   // 채널 개별 수정
@@ -68,12 +77,10 @@ app
             db.set(channelNum,channel)
 
             res.status(200).json({
-                message : `채널명이 정상적으로 수정되었습니다. 기존 "${oldTitle}" -> 수정 "${newTitle}"`
+                message : `채널명이 정상적으로 수정되었습니다. 기존 : ${oldTitle}" -> 수정 : ${newTitle}`
             })
         }else{
-            res.status(404).json({
-                message : "채널 정보를 찾을 수 없습니다."
-            })
+            notFoundChannel()
         }
     }) 
     .delete((req,res)=>{    // 채널 개별 삭제
@@ -87,8 +94,14 @@ app
                 message : `${channel.channelTitle} 채널이 정상적으로 삭제되었습니다.`
             })
         }else{
-            res.status(404).json({
-                message : "채널 정보를 찾을 수 없습니다."
-            })
+            notFoundChannel()
         }
     }) 
+
+function notFoundChannel(){
+    res.status(404).json({
+        message : "채널 정보를 찾을 수 없습니다."
+    })
+}
+
+module.exports = router
--- 웹풀사이클 스프린트 2/youtube-demo/routes/users.js
+++ 웹풀사이클 스프린트 2/youtube-demo/routes/users.js
@@ -1,11 +1,15 @@
 // express 모듈 셋팅
 const express = require('express')
-const app = express()
-app.listen(7777)
-app.use(express.json()) // => req.body를 JSON 형식으로 읽을 수 있게 해주는 모듈
+const router = express.Router(); 
+// app.js에서 router로 접근할 수 있도록 변형
+// 변경 전 : const app = express()
+// 변경 후 : const router = express.Router(); 
+
+router.use(express.json()) 
+// expres.json() : req.body를 JSON 형식으로 읽을 수 있게 해주는 모듈
 
 let db = new Map()
-let accountNum = 1 // 하나의 객체를 유니크하게 구별하기 위함
+// let accountNum = 1 // 하나의 객체를 유니크하게 구별하기 위함
 
 let loginUser = {} 
 // userInfo 선언 시, {} 로 초기화를 했는데 이것은 Null값이기에 undefined가 아님을 항상 주의해야 한다.
@@ -13,23 +17,27 @@ let loginUser = {}
 // checkDB 함수에서 사용하기 위해 로그인 app.post 내부의 변수를 전역변수로 수정
 
 // 로그인
-app.post('/login', (req,res)=>{
+router.post('/login', (req,res)=>{
     const {id, password} = req.body
     
     checkDB(db,id)
     console.log(loginUser)
    
     if(isExist(loginUser)){ 
-        console.log("아이디 같은거 찾았다!")
-
         // password도 맞는지 비교
         if(loginUser.password === password){
-            console.log("패스워드도 같다!")
+            res.status(200).json({
+                message : `${loginUser.name}님 로그인 되었습니다.`
+            })
         }else{
-            console.log("패스워드가 다르다!")
+            res.status(400).json({
+                message : `비밀번호가 틀렸습니다.`
+            })
         }
     }else{
-        console.log("입력하신 아이디는 없는 아이디 입니다.")
+        res.status(404).json({
+            message : `회원정보가 존재하지 않습니다.`
+        })
     }
 })
 
@@ -42,7 +50,6 @@ function checkDB(db, id){
     })
 }
 
-
 // 객체 안에 데이터가 있는지 확인해주는 함수
 function isExist(obj){
     if(Object.keys(obj).length){
@@ -53,13 +60,13 @@ function isExist(obj){
 }
 
 // 회원가입
-app.post('/join', (req,res)=>{
+router.post('/join', (req,res)=>{
     const {id, password, name} = req.body
     if(id && password && name){
-        //console.log(req.body)
-        db.set(accountNum++, req.body)
+        const {id} = req.body
+        db.set(id, req.body)
         res.status(201).json({
-            message : `${db.get(accountNum-1).name}님 환영합니다.`
+            message : `${id}님 환영합니다.`
         }) 
     }else{
         res.status(400).json({
@@ -69,39 +76,42 @@ app.post('/join', (req,res)=>{
 })
 
 // route()를 활용한 REST API 구조 변경
-app.route('/users/:accountNum')
+router
+    .route('/users')
     .get((req,res)=>{   // 회원 개별 조회
-        let {accountNum} = req.params  
-        accountNum = parseInt(accountNum)
-        const user = db.get(accountNum)
+        let {id} = req.body
+        const user = db.get(id)
     
-        if(user == undefined){
-            res.status(404).json({
-                message : `계정번호 ${accountNum}번으로 등록된 유저는 존재하지 않습니다.`
-            })
-        }else{
+        if(user){
             res.status(200).json({
                 id : user.id,
                 name : user.name
             })
+        }else{
+            res.status(404).json({
+                message : `아이디 ${id}로 등록된 유저는 존재하지 않습니다.`
+            })
         }
     })
     .delete((req,res)=>{    // 회원 개별 탈퇴
-        let {accountNum} = req.params  
-        accountNum = parseInt(accountNum)
-        const user = db.get(accountNum)
+        let {id} = req.body
+        const user = db.get(id)
     
-        if(user == undefined){
-            res.status(404).json({
-                message : `계정번호 ${accountNum}번으로 등록된 유저는 존재하지 않습니다.`
-            })
-        }else{
-            db.delete(accountNum)
+        if(user){
+            db.delete(id)
             res.status(200).json({
                 message : `${user.name}님 다음에 또 뵙겠습니다.`
             })
+        }else{
+            res.status(404).json({
+                message : `아이디 ${id}로 등록된 유저는 존재하지 않습니다.`
+            })
         }
     })
+
+
+module.exports = router // 외부 js파일에서 사용할 수 있도록 export 진행
+
 
 // ⬆️⬆️⬆️  route를 활용하여 REST API의 구조를 변경한 방법  ⬆️⬆️⬆️
 // ⬇️⬇️⬇️       기존에 사용했던 REST API 설계방법        ⬇️⬇️⬇️
@@ -141,3 +151,5 @@ app.route('/users/:accountNum')
 //         })
 //     }
 // })
+//
+// module.exports = router // 외부 js파일에서 사용할 수 있도록 export 진행
